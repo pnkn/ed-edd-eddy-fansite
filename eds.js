fafaecd45b92fefcebdb755459b5533238e088d6
pnkn <waynehammond96@gmail.com>
2018-03-01 20:41:00 -0500

    almost done with home page, needs few more things

--- eds.js
+++ eds.js
@@ -1,5 +1,26 @@
+// START slideshow code
+let i = 0;
+let images = [];
+let time = 3000;
+
+images[0] = "img/bag_eds.jpg";
+images[1] = "img/backg2.jpg";
+images[2] = "img/backg.jpg";
+images[3] = "img/squid.jpg";
 
 
+// change image
+function changeImg() {
+	document.slide.src = images[i];
+	
+	if (i < images.length -1 ) {
+		i++;
+	} else {
+		i = 0;
+	}
+	setTimeout("changeImg()", time);
+}
 
-
+window.onload = changeImg;
+// END of slideshow
 
