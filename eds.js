1ca8892bd663eb76a9a7308dc89e31d264812275
pnkn <waynehammond96@gmail.com>
2018-03-02 18:07:52 -0500

    added slideshow controls, changed color scheme and background

--- eds.js
+++ eds.js
@@ -1,12 +1,16 @@
+"use strict";
+
 // START slideshow code
 let i = 0;
 let images = [];
 let time = 3000;
 
 images[0] = "img/bag_eds.jpg";
-images[1] = "img/backg2.jpg";
+images[1] = "img/jimcactus.png";
 images[2] = "img/backg.jpg";
 images[3] = "img/squid.jpg";
+
+
 
 
 // change image
@@ -20,6 +24,30 @@ function changeImg() {
 	}
 	setTimeout("changeImg()", time);
 }
+
+
+// next button
+function nextImage() {
+	if (i < images.length - 1) {
+		i++;
+		document.slide.src = images[i];
+	} else {
+		i = 0;
+		document.slide.src = images[i];
+	}
+}
+
+// previous button
+function prevImage() {
+	if (i != 0) {
+		i--;
+		document.slide.src = images[i];
+	} else {
+		i = images.length - 1;
+		document.slide.src = images[i];
+	}
+}
+
 
 window.onload = changeImg;
 // END of slideshow
